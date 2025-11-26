23a9037c8f367fcfa06ac4262293e5056ff2093e
Akshar Kakumani <53996614+akshar-kakumani@users.noreply.github.com>
2025-11-26 18:18:57 +0530

    a few changes

--- temporalrag/settings.py
+++ temporalrag/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
+DEBUG = True
 
 ALLOWED_HOSTS =["*"]
 
